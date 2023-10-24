7c44625d46e71bdd10bb68679ac0d8e9ae349c38
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-10-24 02:15:01 +0000

    changes

--- rust_runtime/src/main.rs
+++ rust_runtime/src/main.rs
@@ -1,4 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::time::Instant;
 
 fn sum_of_squares(n: u64) -> u64 {
     (1..=n).map(|x| x * x).sum()
@@ -6,10 +7,19 @@ fn sum_of_squares(n: u64) -> u64 {
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Rust Sum of Squares", |b| {
-        b.iter(|| sum_of_squares(black_box(10_000_000)))
+        b.iter(|| {
+            let start_time = Instant::now();
+            let result = sum_of_squares(black_box(10_000_000));
+            let end_time = Instant::now();
+
+            let elapsed_time = end_time - start_time;
+            println!("Start time: {:?}", start_time);
+            println!("End time:   {:?}", end_time);
+            println!("Elapsed time: {:?}", elapsed_time);
+            black_box(result);
+        });
     });
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
-
